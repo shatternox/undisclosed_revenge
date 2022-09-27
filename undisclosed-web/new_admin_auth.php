3c28fdae9a7a39d62af40eefd7ff2661c9f92f0f
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-09-27 11:14:22 +0700

    Some deserialization

--- undisclosed-web/new_admin_auth.php
+++ undisclosed-web/new_admin_auth.php
@@ -1,44 +1,22 @@
 <?php
-include "flag.php";
+include "tokengenerator.php";
 
-show_source(__FILE__);
-
-class user {
-  var $name;
-  var $pass;
-  var $secret;
+class Payment {
+  public $totalPayment;
+  public $token;
 }
 
-if (isset($_GET['id'])) {
+if (isset($_POST['payment_data'])) {
 
-  $id = $_GET['id'];
+  $payment_data = $_POST['payment_data'];
+  $data = unserialize($payment_data);
 
-  $usr = unserialize($id);
-  if ($usr) {
-    $usr->secret = $flag1;
-    if ($usr->name === "admin" && $usr->pass === $usr->secret) {
-      echo "Congratulation! Here is something for you...  " . $usr->pass;
-      if (isset($_GET['caption'])) {
-        $cap = $_GET['caption'];
-        if (strlen($cap) > 45) {
-          die("Naaaah, Take rest now");
-        }
-        if (preg_match("/[A-Za-z0-9]+/", $cap)) {
-          die("Don't mess with the best language!!");
-        }
-        eval($cap);
-        // Try to execute echoFlag()
-      } else {
-        echo "NVM You are not eligible";
-      }
+  if ($data) {
+    if (is_numeric($usr->totalPayment) && $usr->totalPayment > 1 &&  $usr->token === $paymentToken->secrettoken) {
+      echo "Payment is valid!";
     } else {
-      echo "Oh no... You can't fool me";
+      echo "Payment is not valid!";
     }
 
-  } else {
-    echo "are you trolling?";
-  }
-
-} else {
-  echo "Go and watch some Youthoob Tutorials Kidosss!!";
-} 
+  } 
+}
---
+++ undisclosed-web/tokengenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+Class Token {
+    public $secrettoken;
+
+    function __construct(){
+        $this->secrettoken = sha1(uniqid());
+    }
+}
+
+$paymentToken = new Token();
+
+
+?>
